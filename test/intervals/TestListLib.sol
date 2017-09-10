2083392edbc5e0a487221415db8951f70f870ee4
g. nicholas d'andrea <nick@gnidan.org>
2017-09-10 13:20:47 -0400

    Add ListLib test for search hinting behavior

--- test/intervals/TestListLib.sol
+++ test/intervals/TestListLib.sol
@@ -5,6 +5,11 @@ import "truffle/Assert.sol";
 import "../../contracts/intervals/ListLib.sol";
 
 contract TestListLib {
+  // TODO avoid redefinition here
+  uint constant SEARCH_DONE = 0x00;
+  uint constant SEARCH_EARLIER = 0x01;
+  uint constant SEARCH_LATER = 0x10;
+
   using ListLib for ListLib.List;
 
   ListLib.List intervals;
@@ -32,5 +37,58 @@ contract TestListLib {
     Assert.equal(intervals.center, oldCenter, "List center should not change");
     Assert.notEqual(intervals.lowestBegin, oldLowest, "Lowest beginning interval should update");
     Assert.equal(intervals.highestEnd, oldHighest, "Highest ending interval should not update");
+  }
+
+  function test3_searchNext() {
+    /*
+     *   0   1   2   3   4   5   6   7   8   9
+     *                     center
+     *               *-------|-------o
+     *       *---------------|---o
+     *
+     *    search hint behavior:
+     *
+     *                       *
+     *                done with search
+     *
+     *   <:::::::::::::::::::o
+     *       search earlier
+     *                       o:::::::::::::::>
+     *                          search later
+     *
+     *   0   1   2   3   4   5   6   7   8   9
+     */
+
+    uint[] memory matchingIDs;
+    uint8 searchNext;
+
+    /*
+     * case: search done
+     */
+    (matchingIDs, searchNext) = intervals.matching(5);
+    Assert.equal(uint(searchNext), SEARCH_DONE, "Searching overlaps at center finishes search");
+
+    /*
+     * case: search earlier
+     */
+    (matchingIDs, searchNext) = intervals.matching(4);
+    assert(matchingIDs.length > 0);  // interval containing point exists
+    Assert.equal(uint(searchNext), SEARCH_EARLIER, "less than center w/ matches yields SEARCH_EARLIER");
+
+    (matchingIDs, searchNext) = intervals.matching(0);
+    assert(matchingIDs.length == 0);  // no interval containing point
+    Assert.equal(uint(searchNext), SEARCH_EARLIER, "less than center w/o matches yields SEARCH_EARLIER");
+
+    /*
+     * case: search later
+     */
+    (matchingIDs, searchNext) = intervals.matching(6);
+    assert(matchingIDs.length > 0);  // interval containing point exists
+    Assert.equal(uint(searchNext), SEARCH_LATER, "greater than center w/ matches yields SEARCH_LATER");
+
+    (matchingIDs, searchNext) = intervals.matching(7);
+    assert(matchingIDs.length == 0);  // no interval containing point
+    Assert.equal(uint(searchNext), SEARCH_LATER, "greater than center w/o matches yields SEARCH_LATER");
+
   }
 }
