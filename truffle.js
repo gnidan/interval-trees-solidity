ee6db39a826558431c67eac2db98daeae97cc29f
g. nicholas d'andrea <nick@gnidan.org>
2017-09-10 14:00:41 -0400

    Add mainnet config

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 9545,
       network_id: "*" // Match any network id
-    }
+    },
+    main: {
+      host: "localhost",
+      port: 8545,
+      network_id: "1"
+    },
   }
 };
